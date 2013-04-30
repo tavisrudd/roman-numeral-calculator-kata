7fb7690c9fcb51a75454456b08b27e83c8b85485
Jamu Kakar <jkakar@kakar.ca>
2013-04-29 19:41:40 -0700

    - Jamu's efforts.

--- roman/calculator.py
+++ roman/calculator.py
@@ -1,2 +1,26 @@
-# TODO Implement the Roman numeral calculator here.  But look at the tests
-# first, of course.
+class RomanNumeral(object):
+    mapping = {'I': 1,
+               'V': 5,
+               'X': 10,
+               'L': 50,
+               'C': 100,
+               'D': 500,
+               'M': 1000}
+
+    def __init__(self, value):
+        self.value = value
+
+    def to_int(self):
+        return 1
+
+    def _parse(self, value):
+        current = None
+        for char in value.reverse():
+            if current is None:
+                current = self.mapping[char]
+            else:
+                current -= self.mapping[char]
+        return current
+
+
+
--- roman/tests/test_calculator.py
+++ roman/tests/test_calculator.py
@@ -3,4 +3,15 @@ from unittest import TestCase
 from roman.calculator import *
 
 
-# TODO Write your first test here.
+class RomanNumeralTest(TestCase):
+    def test_one(self):
+        one = RomanNumeral('I')
+        self.assertEqual(1, one.to_int())
+
+    def test_four(self):
+        four = RomanNumeral('IV')
+        self.assertEqual(4, four.to_int())
+
+    # def test_fifteen(self):
+    #     fifteen = RomanNumeral('XV')
+    #     self.assertEqual(15, fifteen.to_int())
